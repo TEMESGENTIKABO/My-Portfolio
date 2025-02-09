dfa3cb8df2cfd7eb5f1943f1867803d218675970
TEMESGENTIKABO <temehyaway@gmail.com>
2025-02-09 13:52:25 +0800

    first  commit

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { projects } from "@/data/projects";
+
 import ProjectDetails from "@/components/ProjectDetails";
 import ProjectsShowcase from "@/components/ProjectsShowcase";
 
